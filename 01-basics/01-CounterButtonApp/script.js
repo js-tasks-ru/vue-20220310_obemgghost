1aa21b0a609d60086ed74ae327a12ad1c4ae79c1
obemgcabazn <obemgghost@ya.ru>
2022-04-05 21:04:24 +0300

    feat: 01-basic/01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,18 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+  },
+});
+const counterApp = createApp( App );
+const vm = counterApp.mount('#app');
