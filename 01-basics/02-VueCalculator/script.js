a5f01ee42851ef9f0f735614d8b89762fbb62175
obemgcabazn <obemgghost@ya.ru>
2022-04-05 22:18:03 +0300

    feat: 01-basics/02-VueCalculator, 01-basics/03-MarkedList

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,24 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const AppData = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      valueA: 0,
+      valueB: 0,
+      operator: 'sum',
+    };
+  },
+  computed: {
+    calculateResult() {
+      if (this.operator === 'sum') return this.valueA + this.valueB;
+      if (this.operator === 'subtract') return this.valueA - this.valueB;
+      if (this.operator === 'multiply') return this.valueA * this.valueB;
+      if (this.operator === 'divide') return this.valueA / this.valueB;
+    },
+  },
+});
+
+const App = createApp(AppData).mount('#app');
