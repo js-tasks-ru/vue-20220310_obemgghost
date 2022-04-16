af8103ded39f675805eaf50bd48208e9e6a07e69
obemgcabazn <obemgghost@ya.ru>
2022-04-16 18:56:17 +0300

    feat 02-components/02-MeetupDescription

--- 02-components/02-MeetupDescription/MeetupDescription.js
+++ 02-components/02-MeetupDescription/MeetupDescription.js
@@ -3,5 +3,11 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupDescription',
 
-  template: `<p class="meetup-description">Описание должно быть здесь</p>`,
+  props: {
+    description: {
+      require: false,
+    },
+  },
+
+  template: `<p class="meetup-description">{{ description }}</p>`,
 });
