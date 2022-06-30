8ca4698694f366dd881c5ce354c328251611d748
Electropompa <admin@zilmet.ru>
2022-06-30 16:48:33 +0300

    feat 05-vue-router/03-ScrollBehavior

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -5,7 +5,9 @@ export const router = createRouter({
     if (savedPosition) {
       return savedPosition;
     } else if (to.hash) {
-      return { selector: to.hash };
+      return { el: to.hash };
+    } else if (to.meta.saveScrollPosition && from.meta.saveScrollPosition ) {
+      return false;
     } else {
       return { top: 0, left: 0 };
     }
