764f0fffd186403bab968091e4d1c143736e3142
Electropompa <admin@zilmet.ru>
2022-07-08 15:41:09 +0300

    feat: 06-wrappers/04-UiCheckbox

--- 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
+++ 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
@@ -1,14 +1,34 @@
 <template>
   <label class="checkbox">
-    <input type="checkbox" class="checkbox__input" />
+    <input v-model="checkboxValue" v-bind="$attrs" :value="value" type="checkbox" class="checkbox__input" />
     <span class="checkbox__box"></span>
-    Label Text
+    <slot></slot>
   </label>
 </template>
 
 <script>
 export default {
   name: 'UiCheckbox',
+
+  inheritAttrs: false,
+
+  props: {
+    modelValue: [Boolean, Object],
+    value: String,
+  },
+
+  emits: [ 'update:modelValue' ],
+
+  computed: {
+    checkboxValue: {
+      get(){
+        return this.modelValue;
+      },
+      set(newValue){
+        this.$emit('update:modelValue', newValue);
+      }
+    }
+  }
 };
 </script>
 
